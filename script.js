862cc1c1d4c378f12d68186ce253454d4b72ba76
suraj <suraj@techuz.com>
2024-04-23 15:47:35 +0530

    completed

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("yesBtn").addEventListener("click", function() {
+        alert("Yay! Time to squad up 🎉");
+    });
+
+    document.getElementById("noBtn").addEventListener("click", function() {
+        alert("No worries! Maybe another time 😊");
+    });
+});
